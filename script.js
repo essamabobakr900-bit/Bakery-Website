a21dc80404c853bfdf95e302af97c1b6617e14fb
bakr27072008 <essamabobakr900@gmail.com>
2026-06-11 06:04:05 +0300

    I wrote the variable and organized the structure of the whole project.

---
+++ script.js
@@ -0,0 +1,16 @@
+/*=========== SHOW MENU ========*/
+
+
+/*=========== REMOVE MENU MOBILE ======*/
+
+
+/*======= ADD BLUR HEADER =======*/
+
+
+/*================ SHOW SCROLL UP ============*/
+
+
+/*======= SCROLL SECTION ACTIVE LINK ======*/
+
+
+/*============ SCROLL REVEAL ANIMATION ==============*/
